02f059fcfb97a4324304f930fa0fc1f34d4f7a49
geor0014 <geor0014@hz.nl>
2024-05-16 19:42:25 +0900

    8 - destructuring props

--- src/index.js
+++ src/index.js
@@ -56,15 +56,15 @@ function App() {
   );
 }
 
-function Pizza(props) {
-  if (props.pizzaObj.soldOut) return null;
+function Pizza({ pizzaObj }) {
+  if (pizzaObj.soldOut) return null;
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price}</span>
       </div>
     </li>
   );
@@ -115,12 +115,10 @@ function Footer() {
   );
 }
 
-function Order(props) {
+function Order({ closeHour }) {
   return (
     <div className="order">
-      <p>
-        We are open until {props.closeHour}:00. Come visit us or order online
-      </p>
+      <p>We are open until {closeHour}:00. Come visit us or order online</p>
       <button className="btn">Order now</button>
     </div>
   );
